0963ca68fd5dd52e830155cd1df381c665bddbb9
abhi230104001 <abhishek2304001@gmail.com>
2025-12-18 20:53:49 +0530

    fix

--- components/EditSnippetForm.tsx
+++ components/EditSnippetForm.tsx
@@ -1,38 +1,40 @@
 "use client";
+
 import { Editor } from "@monaco-editor/react";
 import React, { useState } from "react";
-import type { Snippet } from "@prisma/client";
 import { Button } from "./ui/button";
 import { saveSnippet } from "@/actions";
 
+type Snippet = {
+  id: number;
+  code: string;
+};
 
 const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
   const [code, setCode] = useState(snippet.code);
 
-  const changeEvenHandler = (value:string = "") => {
+  const changeEventHandler = (value: string = "") => {
     setCode(value);
-  }
-
-  // you can't use server action as a inline inside client component
-  //   async function saveSnippet () {
-  //     "use server"
-
-  //   }
+  };
 
   const saveSnippetAction = saveSnippet.bind(null, snippet.id, code);
 
   return (
     <div className="flex flex-col gap-4">
-      <form action={saveSnippetAction} className="flex items-center justify-between">
+      <form
+        action={saveSnippetAction}
+        className="flex items-center justify-between"
+      >
         <h1 className="font-bold text-xl">Your Code Editor:</h1>
         <Button type="submit">Save</Button>
       </form>
+
       <Editor
         height="40vh"
         theme="vs-dark"
         defaultLanguage="javascript"
         defaultValue={code}
-        onChange={changeEvenHandler}
+        onChange={changeEventHandler}
       />
     </div>
   );
